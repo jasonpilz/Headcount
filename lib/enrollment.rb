54a6b98dacd274ab36bed7107965ce2d18eceac2
Jason Pilz <jasonpilz@gmail.com>
2015-09-17 11:56:01 -0600

    Passing 6 enrollment tests

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -1,6 +1,7 @@
 require_relative './input_files'
 require_relative './enrollment_parser'
 require 'pry'
+
 class Enrollment
   attr_reader :name
 
@@ -11,10 +12,11 @@ class Enrollment
   def dropout_rate_in_year(year)
     # loader?
     dropout_rates = EnrollmentParser.parse(@name, InputFiles::DROPOUT_RATES)
-
     # parser class/method?
     row = dropout_rates.select { |row| row if row[:timeframe].to_i == year && row[:category] == "All Students" }
     rate = row[0][:data].to_f unless row.empty?
+    # return a truncated 3-digit float
+    truncated_rate = rate.round(3) if rate != nil
   end
 
   def dropout_rate_by_gender_in_year(year)
@@ -28,6 +30,6 @@ class Enrollment
       result[:male] = row[:data].to_f if row[:category] == "Male Students"
       result[:female] = row[:data].to_f if row[:category] == "Female Students"
     end
-    result
+    return result unless result == {}
   end
 end
--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -4,6 +4,7 @@ class EnrollmentTest < Minitest::Test
 
   def setup
     @enrollment1 = Enrollment.new('ACADEMY 20')
+    @enrollment2 = Enrollment.new('Colorado')
   end
 
   def test_take_district_name_by_default
@@ -12,14 +13,16 @@ class EnrollmentTest < Minitest::Test
 
   def test_returns_dropout_rate_for_all_students_in_given_year
     assert_equal 0.002, @enrollment1.dropout_rate_in_year(2011)
+    assert_equal 0.03, @enrollment2.dropout_rate_in_year(2011)
   end
 
-  def test_dropout_rate_returns_nil_for_unknown_year
+  def test_dropout_rate_in_year_returns_nil_for_unknown_year
     assert_nil @enrollment1.dropout_rate_in_year(1)
   end
 
-  def test_dropout_rate_returns_three_digit_float
-    skip
+  def test_dropout_rate_in_year_returns_three_digit_float
+    assert_equal 0.030, @enrollment2.dropout_rate_in_year(2011)
+    assert_equal 0.004, @enrollment1.dropout_rate_in_year(2012)
   end
 
   def test_dropout_rate_by_gender_in_year_returns_a_hash_with_genders_as_keys_pointing_to_floats
@@ -28,6 +31,6 @@ class EnrollmentTest < Minitest::Test
   end
 
   def test_dropout_rate_by_gender_in_year_returns_nil_for_unknown_year
-    skip
+    assert_nil @enrollment1.dropout_rate_by_gender_in_year(2008)
   end
 end
