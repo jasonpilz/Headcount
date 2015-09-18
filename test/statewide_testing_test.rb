ef3c17f30b5111a3085a87b2136ce88e7c6cd337
Pat Wey <patrickwey@gmail.com>
2015-09-18 17:07:25 -0600

    changed test names

--- test/statewide_testing_test.rb
+++ test/statewide_testing_test.rb
@@ -10,7 +10,7 @@ class StatewideTestingTest < Minitest::Test
   end
 
   def test_it_takes_district_name_by_default
-    assert_equal 'ACADEMY 20', @statewide1.name 
+    assert_equal 'ACADEMY 20', @statewide1.name
   end
 
   def test_proficient_by_grade_returns_a_hash_with_years_pointing_to_hashes_of_subjects
@@ -25,15 +25,15 @@ class StatewideTestingTest < Minitest::Test
     skip
   end
 
-  def test_proficient_by_race_returns_hash_with_years_pointing_to_hashes_of_subjects
+  def test_proficient_by_race_or_ethnicity_returns_hash_with_years_pointing_to_hashes_of_subjects
     skip
   end
 
-  def test_proficient_by_race_returns_UnknownDataError_for_unknown_race
+  def test_proficient_by_race_or_ethnicity_returns_UnknownDataError_for_unknown_race
     skip
   end
 
-  def test_proficient_by_race_returns_percentage_values_as_three_digit_floats
+  def test_proficient_by_race_or_ethnicity_returns_percentage_values_as_three_digit_floats
     skip
   end
 
