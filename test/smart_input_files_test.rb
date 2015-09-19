d34fe34ad0aaed9a1dd8c53851f5d5d93cfc7206
Pat Wey <patrickwey@gmail.com>
2015-09-19 16:49:06 -0600

    created smart_input_files class & test file

---
+++ test/smart_input_files_test.rb
@@ -0,0 +1,9 @@
+class SmartInputFilesTest < Minitest::Test
+  def test_it_loads_in_all_files_from_a_directory
+    filenames = [
+                
+                ]
+  end
+  # def test_it_sorts_all_files_based_on_keywords_in_filenames
+  # def test_it_stores_those_files_in_constants
+end
