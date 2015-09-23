b4b7aeefb847a1cfa88900c70d407b84e94bd1c7
Pat Wey <patrickwey@gmail.com>
2015-09-23 16:50:28 -0600

    refactored economic_profile

--- lib/economic_profile.rb
+++ lib/economic_profile.rb
@@ -1,69 +1,68 @@
-require 'pry'
-
 class EconomicProfile
-  attr_reader :name, :parser
+  attr_reader :name
 
   def initialize(name, parser)
     @name = name
     @parser = parser
   end
 
-  def free_or_reduced_lunch_by_year
-    lunch_by_year = @parser.parse_reduced_lunch(@name)
-    lunch_percent = lunch_by_year.select do |row|
-      row if (row[:dataformat] == "percent") && (row[:poverty_level] == "eligible for free or reduced lunch")
-    end
-    lunch_percent.empty? ? return : result = {}
-    lunch_percent.each do |row|
-      result[row[:timeframe].to_i] = row[:data][0..4].to_f
+  def truncate(string)
+    string[0..4].to_f
+  end
+
+  def data_by_year(rows, result = {})
+    rows.each do |row|
+      result[row[:timeframe].to_i] = truncate(row[:data])
     end
     result
   end
 
-  def free_or_reduced_lunch_in_year(year)
-    lunch_by_year = @parser.parse_reduced_lunch(@name)
-    lunch_in_year = lunch_by_year.select do |row|
-      row if (row[:dataformat] == "percent") && (row[:poverty_level] == "eligible for free or reduced lunch") && (row[:timeframe].to_i == year)
+  def free_or_reduced_lunch_by_year
+    lunch = @parser.parse_reduced_lunch(@name)
+    lunch_percents = lunch.select do |row|
+      row if (row[:dataformat] == 'percent') &&
+             (row[:poverty_level] == 'eligible for free or reduced lunch')
     end
-    lunch_in_year.empty? ? return : lunch_in_year.first[:data][0..4].to_f
+    lunch_percents.empty? ? {} : data_by_year(lunch_percents)
+  end
+
+  def free_or_reduced_lunch_in_year(year)
+    lunch = @parser.parse_reduced_lunch(@name)
+    lunch_in_year = lunch.select do |row|
+      row if (row[:dataformat] == 'percent') &&
+             (row[:poverty_level] == 'eligible for free or reduced lunch') &&
+             (row[:timeframe].to_i == year)
+    end
+    lunch_in_year.empty? ? return : truncate(lunch_in_year.first[:data])
   end
 
   def school_aged_children_in_poverty_by_year
     children = @parser.parse_children_in_poverty(@name)
-    children_by_year = children.select do |row|
+    children_percents = children.select do |row|
       row if row[:dataformat] == 'percent'
     end
-    result = {}
-    return result if children_by_year.empty?
-    children_by_year.each do |row|
-      result[row[:timeframe].to_i] = row[:data][0..4].to_f
-    end
-    result
+    children_percents.empty? ? {} : data_by_year(children_percents)
   end
 
   def school_aged_children_in_poverty_in_year(year)
     children = @parser.parse_children_in_poverty(@name)
-    children_by_year = children.select do |row|
+    children_in_year = children.select do |row|
       row if row[:dataformat] == 'percent' && row[:timeframe].to_i == year
     end
-    children_by_year.empty? ? return : children_by_year.first[:data][0..4].to_f
+    children_in_year.empty? ? return : truncate(children_in_year[0][:data])
   end
 
   def title_1_students_by_year
     title_1 = @parser.parse_title_1(@name)
-    result = {}
-    students_by_year = title_1.select do |row|
-      result[row[:timeframe].to_i] = row[:data][0..4].to_f if row[:dataformat] == 'percent'
-    end
-    result
+    students = title_1.select { |row| row if row[:dataformat] == 'percent' }
+    data_by_year(students)
   end
 
   def title_1_students_in_year(year)
     title_1 = @parser.parse_title_1(@name)
     title_1_in_year = title_1.select do |row|
-        row if row[:dataformat] == 'percent' && row[:timeframe].to_i == year
+      row if row[:dataformat] == 'percent' && row[:timeframe].to_i == year
     end
-    title_1_in_year.empty? ? return : title_1_in_year.first[:data][0..4].to_f
+    title_1_in_year.empty? ? return : truncate(title_1_in_year[0][:data])
   end
-
 end
